77873c3c04564804ed37e94cc4910fc1507b5a92
arthurcai54 <arthurcai54@gmail.com>
2022-12-04 17:35:33 -0800

    updated receiver app.py file

--- Receiver/app.py
+++ Receiver/app.py
@@ -10,6 +10,7 @@ import logging
 import logging.config
 import uuid
 from pykafka import KafkaClient
+import time
 
 with open('app_conf.yml', 'r') as f:
     app_config = yaml.safe_load(f.read())
@@ -54,6 +55,24 @@ def sales(body):
     producer.produce(msg_str.encode('utf-8'))
 
     return f"{body['profits']:.2f} made", 201
+
+def process_messages(): 
+    """ Process event messages """ 
+    hostname = "%s:%d" % (app_config["events"]["hostname"],   
+                          app_config["events"]["port"]) 
+    max_num_retries = 0
+    current_retry_count = 0
+
+    while current_retry_count < max_num_retries:
+        try:
+            logger.info("Attempting to connect to Kafa and the current retry count")
+            client = KafkaClient(hosts=hostname) 
+            topic = client.topics[str.encode(app_config["events"]["topic"])] 
+        except:
+            logger.error("connection failed")
+            time.sleep()
+            current_retry_count += 1
+
 
 app = connexion.FlaskApp(__name__, specification_dir='')
 app.add_api("amazonAPI.yaml",
